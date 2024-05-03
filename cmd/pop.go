de5aea89bb94412bcee30b8c725d0d548cee3296
sugy <sugy.kz@gmail.com>
2024-05-03 16:15:29 +0900

    Updated command descriptions.

--- cmd/pop.go
+++ cmd/pop.go
@@ -14,13 +14,8 @@ import (
 // popCmd represents the pop command
 var popCmd = &cobra.Command{
 	Use:   "pop",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "pop queue (dequeue)",
+	Long:  `Retrieve and execute stored queues.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("pop called")
 	},
