e8f0b08b7729a842dba9caf6326d1e19e5717175
sugy <sugy.kz@gmail.com>
2024-05-21 00:10:53 +0900

    modified dequeue.
    - Do not return err if an error occurs in processing the queue cntents
    when dequeue.

--- internal/queue.go
+++ internal/queue.go
@@ -127,7 +127,7 @@ func (f *FileQueue) Dequeue() error {
 		rc, err := f.Dir.Open(key)
 		if err != nil {
 			log.Fatal(fmt.Sprintf("Error opening file: %v\n", err))
-			return err
+			return nil
 		}
 		defer rc.Close()
 
@@ -153,27 +153,28 @@ func (f *FileQueue) Dequeue() error {
 		msg, err := base64.StdEncoding.DecodeString(q.Payload.Massage)
 		if err != nil {
 			log.Fatal(fmt.Sprintf("Error: base64 decoding: %v", err))
-			return err
+			return nil
 		}
 
 		switch q.Payload.Kind {
 		case "exec":
+			log.Debug("kind: exec")
 			cmdStr := strings.Fields(os.ExpandEnv(string(msg)))
 			exec := newExecute(cmdStr[0], cmdStr[1:])
 			err = exec.run()
 			if err != nil {
-				log.Fatal(err)
-				return err
+				log.Fatal(fmt.Sprintf("Error command execute: %v\n", err))
+				return nil
 			}
 			log.Info(fmt.Sprintf("execute command. exitCode: %d, stdout: '%s', stderr: '%s'\n",
 				exec.exitCode, exec.stdout, exec.stderr))
 		case "clipboard":
-			log.Info("clipboard!")
+			log.Debug("kind: clipboard")
 			clip := newClipboard("")
 			err := clip.copy(msg)
 			if err != nil {
-				log.Fatal(err)
-				return err
+				log.Fatal(fmt.Sprintf("Error clipboard: %v\n", err))
+				return nil
 			}
 		}
 
