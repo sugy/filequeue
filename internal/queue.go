ad02092b124bff950030a960af3ee36d2a09a355
sugy <sugy.kz@gmail.com>
2024-05-25 17:24:14 +0900

    add test for queue.

--- internal/queue.go
+++ internal/queue.go
@@ -72,8 +72,7 @@ func (f *FileQueue) setupFileQueuedir() error {
 func (f *FileQueue) Enqueue(k string, m string) error {
 	log.Debug("enqueue!")
 	if !validateKind(k) {
-		errMsg := "This string is not in the kind."
-		log.Fatal(errMsg)
+		errMsg := "this string is not in the kind"
 		return errors.New(errMsg)
 	}
 
@@ -84,21 +83,21 @@ func (f *FileQueue) Enqueue(k string, m string) error {
 
 	yamlBytes, err := yaml.Marshal(q)
 	if err != nil {
-		log.Fatal(fmt.Sprintf("Error marshaling to YAML: %v", err))
+		return err
 	}
 
 	d, err := maildir.NewDelivery(string(f.Dir))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	b, err := d.Write(yamlBytes)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	log.Debug(fmt.Sprintf("deliverd bytes: %v", b))
 	err = d.Close()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	return nil
 }
@@ -109,12 +108,13 @@ func (f *FileQueue) Dequeue() error {
 	news, err := f.Dir.Unseen()
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Error maildir.Dir.Unseen: %v", err))
+		return err
 	}
+	log.Debug(fmt.Sprintf("new keys: %v", news))
 
 	if len(news) == 0 {
 		return nil
 	}
-	log.Info(fmt.Sprintf("new keys: %v", news))
 
 	err = f.Dir.Walk(func(key string, flags []maildir.Flag) error {
 		log.Debug(fmt.Sprintf("%v, %v", key, flags))
@@ -190,7 +190,13 @@ func (f *FileQueue) Dequeue() error {
 // Purge is...
 func (f *FileQueue) Purge() error {
 	log.Debug("purge!")
-	err := f.Dir.Walk(func(key string, flags []maildir.Flag) error {
+	_, err := f.Dir.Unseen()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error maildir.Dir.Unseen: %v", err))
+		return err
+	}
+
+	err = f.Dir.Walk(func(key string, flags []maildir.Flag) error {
 		log.Debug(fmt.Sprintf("%v, %v", key, flags))
 
 		err := f.Dir.Remove(key)
---
+++ internal/queue_test.go
@@ -0,0 +1,195 @@
+package filequeue
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"gopkg.in/yaml.v3"
+)
+
+func TestNewFileQueue(t *testing.T) {
+	tests := []struct {
+		name string
+		dir  string
+	}{
+		{name: "Create New Queue", dir: "filequeue_test"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", tt.dir)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(dir)
+
+			fq := NewFileQueue(dir)
+			if fq == nil {
+				t.Fatal("Expected non-nil FileQueue")
+			}
+
+			if _, err := os.Stat(filepath.Join(dir, "new")); os.IsNotExist(err) {
+				t.Fatalf("Expected 'new' directory to be created")
+			}
+		})
+	}
+}
+
+func TestEnqueue(t *testing.T) {
+	tests := []struct {
+		name    string
+		kind    string
+		message string
+		wantErr bool
+	}{
+		{name: "Valid Exec Message", kind: "exec", message: "echo hello", wantErr: false},
+		{name: "Valid Clipboard Message", kind: "clipboard", message: "copy this", wantErr: false},
+		{name: "Invalid Kind", kind: "invalid", message: "invalid kind", wantErr: true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "filequeue_test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(dir)
+
+			fq := NewFileQueue(dir)
+			if fq == nil {
+				t.Fatal("Expected non-nil FileQueue")
+			}
+
+			err = fq.Enqueue(tt.kind, tt.message)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("Expected error: %v, got: %v", tt.wantErr, err)
+			}
+
+			if !tt.wantErr {
+				keys, err := fq.Dir.Unseen()
+				if err != nil {
+					t.Fatalf("Expected no error, got %v", err)
+				}
+
+				if len(keys) == 0 {
+					t.Fatalf("Expected at least one message in the queue")
+				}
+
+				rc, err := fq.Dir.Open(keys[0])
+				if err != nil {
+					t.Fatalf("Expected no error, got %v", err)
+				}
+				defer rc.Close()
+
+				var q queue
+				fileContent, err := io.ReadAll(rc)
+				if err != nil {
+					t.Fatalf("Expected no error, got %v", err)
+				}
+
+				if err := yaml.Unmarshal(fileContent, &q); err != nil {
+					t.Fatalf("Expected no error, got %v", err)
+				}
+
+				if q.Payload.Kind != tt.kind {
+					t.Fatalf("Expected '%v' kind, got %v", tt.kind, q.Payload.Kind)
+				}
+			}
+		})
+	}
+}
+
+func TestDequeue(t *testing.T) {
+	tests := []struct {
+		name    string
+		kind    string
+		message string
+		wantErr bool
+	}{
+		{name: "Dequeue Exec Message", kind: "exec", message: "echo hello", wantErr: false},
+		{name: "Dequeue Clipboard Message", kind: "clipboard", message: "copy this", wantErr: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "filequeue_test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(dir)
+
+			fq := NewFileQueue(dir)
+			if fq == nil {
+				t.Fatal("Expected non-nil FileQueue")
+			}
+
+			err = fq.Enqueue(tt.kind, tt.message)
+			if err != nil {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+
+			err = fq.Dequeue()
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("Expected error: %v, got: %v", tt.wantErr, err)
+			}
+
+			keys, err := fq.Dir.Unseen()
+			if err != nil {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+
+			if len(keys) != 0 {
+				t.Fatalf("Expected no messages in the queue")
+			}
+		})
+	}
+}
+
+func TestPurge(t *testing.T) {
+	tests := []struct {
+		name    string
+		kind    string
+		message string
+		wantErr bool
+	}{
+		{name: "Purge Exec Message", kind: "exec", message: "echo hello", wantErr: false},
+		{name: "Purge Clipboard Message", kind: "clipboard", message: "copy this", wantErr: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "filequeue_test")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(dir)
+
+			fq := NewFileQueue(dir)
+			if fq == nil {
+				t.Fatal("Expected non-nil FileQueue")
+			}
+
+			err = fq.Enqueue(tt.kind, tt.message)
+			if err != nil {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+
+			err = fq.Purge()
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("Expected error: %v, got: %v", tt.wantErr, err)
+			}
+
+			keys, err := fq.Dir.Unseen()
+			if err != nil {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+
+			if len(keys) != 0 {
+				t.Error(keys)
+				t.Fatalf("Expected no messages in the queue")
+			}
+		})
+	}
+}
