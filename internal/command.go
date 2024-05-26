b3f86f45c73ec2573d9bfa430e84131f0c5b9c8d
sugy <sugy.kz@gmail.com>
2024-05-26 10:27:33 +0900

    add CommandExecutor interface.

---
+++ internal/command.go
@@ -0,0 +1,18 @@
+// Package filequeue ...
+package filequeue
+
+import "os/exec"
+
+// CommandExecutor is an interface that wraps the exec.Command function.
+type CommandExecutor interface {
+	Command(name string, arg ...string) *exec.Cmd
+}
+
+type realCommandExecutor struct{}
+
+func (r *realCommandExecutor) Command(name string, arg ...string) *exec.Cmd {
+	return exec.Command(name, arg...)
+}
+
+// CmdExec ...
+var CmdExec CommandExecutor = &realCommandExecutor{}
